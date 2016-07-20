20d3d598e672ca355a380844b84a3befabb92d82
dheenupriyarasu <deenupriyar@gmail.com>
2016-07-20 12:17:23 +0530

    Create Positive .java

---
+++ Positive .java
@@ -0,0 +1,25 @@
+# dheenupriya
+import java.util.Scanner;
+
+
+public class Positive {
+	static int a;
+	static Scanner scn=new Scanner(System.in);
+	
+	public static void main(String args[]){
+	int n = scn.nextInt();
+	if(a>0)
+		System.out.println("positive");
+	else if(a<0)
+	{
+		
+	System.out.println("negative");
+		
+     
+
+	
+	
+}
+
+	}
+}
