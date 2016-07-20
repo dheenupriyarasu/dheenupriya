afef987d678670f252f230f0917164e65cb2d482
dheenupriyarasu <deenupriyar@gmail.com>
2016-07-20 12:32:31 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,17 @@
+# dheenupriya
+public class Even {
+	public static void main(String args[]){
+		int a=4;int b=31;
+		if(a%2==0)
+		{
+System.out.println("even");
+		}
+			else if(b%2==1)
+			{
+				System.out.println("odd");	
+			}
+				
+	}}
+
+					
+		
